eb2a0e86bf3f367e89f5e5ed771e2e6ea2e77efe
kimsolee95 <ahhasolee@naver.com>
2022-12-29 17:55:20 +0900

    jdbc 사용하여 mysql db crud

---
+++ src/main/java/zerobase/weather/domain/Memo.java
@@ -0,0 +1,18 @@
+package zerobase.weather.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+
+  private int id;
+  private String text;
+
+}
